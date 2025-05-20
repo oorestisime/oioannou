09f7977e7de763542b45bcb7405bd2d8b9e40b59
Orestis Ioannou <oorestisime@gmail.com>
2025-05-20 12:21:51 +0300

    Fix sitemap build and redirects

--- app/lib/sitemap.ts
+++ app/lib/sitemap.ts
@@ -11,7 +11,7 @@ const SITE_URL = "https://oioannou.com"
 export function generateSitemap() {
   // Get all blog posts
   const posts = getSortedPostsData()
-  
+
   // Start XML content
   let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -34,10 +34,10 @@ export function generateSitemap() {
 
   // Add each blog post to the sitemap
   posts.forEach((post) => {
-    const postPath = post.path.startsWith('/') ? post.path : `/${post.path}`
+    const postPath = post.path.startsWith("/") ? post.path : `/${post.path}`
     sitemap += `  <url>
-    <loc>${SITE_URL}${postPath}</loc>
-    <lastmod>${new Date(post.date).toISOString().split('T')[0]}</lastmod>
+    <loc>${SITE_URL}/blog${postPath}</loc>
+    <lastmod>${new Date(post.date).toISOString().split("T")[0]}</lastmod>
     <priority>0.7</priority>
   </url>
 `
@@ -49,6 +49,6 @@ export function generateSitemap() {
   // Write sitemap to public directory
   const sitemapPath = path.join(process.cwd(), "public/sitemap.xml")
   fs.writeFileSync(sitemapPath, sitemap)
-  
+
   console.log(`Sitemap generated at ${sitemapPath}`)
 }
